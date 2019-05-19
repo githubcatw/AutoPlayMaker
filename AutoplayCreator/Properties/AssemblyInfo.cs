f95936e1d2ef4a1821cb3e19180be457b6ef8a15
Narek Torosyan <narek.email@gmail.com>
2019-05-19 17:41:10 +0400

    Add signing
    Debug builds are not signed, and releases are signed by me.

--- AutoplayCreator/Properties/AssemblyInfo.cs
+++ AutoplayCreator/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoPlay Maker")]
-[assembly: AssemblyDescription("AutoPlay creation tool")]
+[assembly: AssemblyDescription("A simple AutoPlay creation tool")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NUDev")]
 [assembly: AssemblyProduct("AutoPlay Maker")]
